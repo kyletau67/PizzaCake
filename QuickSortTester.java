bfeac946d1e46c3d9552942fb8026a25cd0d7e18
kyle tau <ktau@stuy.edu>
2018-03-14 07:55:53 -0400

    testing

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -1,10 +1,13 @@
 public class QuickSortTester{
     
     public static void main(String[] args) {
-	startTime = System.currentTimeMillis();
-	//run quicksort tests in between here
-	endTime = System.currentTimeMillis();
-	System.out.println("Time elapsed: "+(endTime - startTime));
+	for (int i = 0; i < 10000; i++) {
+	    int[] arr = QuickSort.buildArray(i,500);
+	    QuickSort.shuffle(arr);
+	    double startTime = System.nanoTime();
+	    QuickSort.qsort(arr);
+	    double endTime = System.nanoTime();
+	    System.out.println(endTime - startTime);
+	}
     }
-    
 }
