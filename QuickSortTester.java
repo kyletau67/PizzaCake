77f5b4e49bed08352e01def02d51b5e03d7fed79
kyle tau <ktau@stuy.edu>
2018-03-13 20:19:04 -0400

    skeleton for tester

---
+++ QuickSortTester.java
@@ -0,0 +1,10 @@
+public class QuickSortTester{
+    
+    public static void main(String[] args) {
+	startTime = System.currentTimeMillis();
+	//run quicksort tests in between here
+	endTime = System.currentTimeMillis();
+	System.out.println("Time elapsed: "+(endTime - startTime));
+    }
+    
+}
